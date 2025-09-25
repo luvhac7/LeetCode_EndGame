75c7a24aca9ca21819a9426cae884ff247622f2c
Luv <ks753159@gmail.com>
2025-09-25 19:10:35 +0530

    Time: 4 ms (7.22%), Space: 12.8 MB (72.83%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        //simple dp aproch
+        int n=triangle.size();
+        vector<int>dp=triangle.back();
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+             {
+                dp[j]=triangle[i][j]+min(dp[j],dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+};
