96389890ee9aece5905385f3ccf326b77bd9ddb4
Luv <ks753159@gmail.com>
2025-11-25 15:59:13 +0530

    Time: 0 ms (100%), Space: 3.8 MB (100%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.go
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.go
@@ -1,13 +1,13 @@
 func smallestRepunitDivByK(k int) int {
-if k%2 ==0 || k%5 ==0{
-    return -1
-}
-rem:=0
-for l:=1;l<=k;l++{
-    rem=(rem*10+1)%k
-    if rem==0{
-        return l
-    }
-}
-return -1
-}
+        if k%2 ==0 || k%5 ==0{
+            return -1
+            }
+            rem:=0
+            for l:=1;l<=k;l++{
+                rem=(rem*10+1)%k
+                    if rem==0{
+                            return l
+                                }
+                                }
+                                return -1
+                                }
