b7e293516683e67320450106a8ca3f9e700f87de
Luv <ks753159@gmail.com>
2025-11-26 10:17:46 +0530

    Time: 26 ms (100%), Space: 10.7 MB (100%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.go
@@ -0,0 +1,37 @@
+func numberOfPaths(grid [][]int, k int) int {
+
+
+        	const mod int = 1e9 + 7
+
+            	gridHeight, gridWidth := len(grid), len(grid[0])
+                	getCell := func(y, x int) int { return grid[y][x] }
+                    	if gridHeight < gridWidth {
+                        		gridWidth, gridHeight = gridHeight, gridWidth
+                                		getCell = func(y, x int) int { return grid[x][y] }
+                                        	}
+
+                                            	curPathCounts := make([][]int, gridWidth)
+                                                	prevPathCounts := make([][]int, gridWidth)
+                                                    	for x := range gridWidth {
+                                                        		curPathCounts[x] = make([]int, k)
+                                                                		prevPathCounts[x] = make([]int, k)
+                                                                        	}
+                                                                                prevPathCounts[0][0] = 1
+
+                                                                                	for y := range gridHeight {
+                                                                                    		cell := getCell(y, 0) % k
+                                                                                            		for i := range k {
+                                                                                                    			curPathCounts[0][(i+cell)%k] = prevPathCounts[0][i] % mod
+                                                                                                                		}
+
+                                                                                                                        		for x := 1; x < gridWidth; x++ {
+                                                                                                                                			cell := getCell(y, x) % k
+                                                                                                                                            			for i := range k {
+                                                                                                                                                        				curPathCounts[x][(i+cell)%k] =
+                                                                                                                                                                        					(prevPathCounts[x][i] + curPathCounts[x-1][i]) % mod
+                                                                                                                                                                                            			}
+                                                                                                                                                                                                        		}
+                                                                                                                                                                                                                		prevPathCounts, curPathCounts = curPathCounts, prevPathCounts
+                                                                                                                                                                                                                        	}
+                                                                                                                                                                                                                                return prevPathCounts[gridWidth-1][0]
+                                                                                                                                                                                                                                }
