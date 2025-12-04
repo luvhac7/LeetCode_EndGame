d691d17b112e694fee2a8c3560e085352bd1c943
Luv <ks753159@gmail.com>
2025-12-04 12:57:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (100%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.go
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.go
@@ -1,16 +1,16 @@
 func countCollisions(directions string) int {
-        i, j := 0, len(directions)-1
-            for i < len(directions) && directions[i] == 'L' {
-                    i++
-                        }
-                            for j >= 0 && directions[j] == 'R' {
-                                    j--
-                                        }
-                                            count := 0
-                                                for k := i; k <= j; k++ {
-                                                        if directions[k] != 'S' {
-                                                                    count++
-                                                                            }
-                                                                                }
-                                                                                    return count
-                                                                                    }
+            i, j := 0, len(directions)-1
+                        for i < len(directions) && directions[i] == 'L' {
+                                            i++
+                                                                    }
+                                                                                                for j >= 0 && directions[j] == 'R' {
+                                                                                                                                    j--
+                                                                                                                                                                            }
+                                                                                                                                                                                                                        count := 0
+                                                                                                                                                                                                                                                                        for k := i; k <= j; k++ {
+                                                                                                                                                                                                                                                                                                                                if directions[k] != 'S' {
+                                                                                                                                                                                                                                                                                                                                                                                                    count++
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    return count
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
