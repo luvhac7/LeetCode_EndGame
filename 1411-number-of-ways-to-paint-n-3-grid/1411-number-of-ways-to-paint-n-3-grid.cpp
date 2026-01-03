5df43e9530605aad0d7807f37620ec206ae6a834
Luv <ks753159@gmail.com>
2026-01-03 12:04:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-typedef long long ll;
+    typedef long long ll;
     int numOfWays(int n) {
-        const int m=1e9+7;
-        ll a=6,b=6;
-        for(int i=2;i<=n;i++){
-            ll aa=(a*2+2*b)%m;
-            ll bb=(2*a+3*b)%m;
-            a=aa;
-            b=bb;
+        const int m = 1e9 + 7;
+        ll a = 6, b = 6;
+        for (int i = 2; i <= n; i++) {
+            ll aa = (a * 2 + 2 * b) % m;
+            ll bb = (2 * a + 3 * b) % m;
+            a = aa;
+            b = bb;
         }
-        return (a+b)%m;
+        return (a + b) % m;
     }
 };
