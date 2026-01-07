ecaa6836b27f1362fa90aa4a9d860e9b5024118c
Luv <ks753159@gmail.com>
2026-01-07 07:29:29 +0530

    Time: 623 ms (5.16%), Space: 85.7 MB (15.49%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,34 +1,35 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int ts=0;
-    long res=0;
-    int maxProduct(TreeNode* root)
-    {
-        ts=getts(root);
-        getss(root);
-        return res%(int)(1e9+7);
-    }
-    int getts(TreeNode* node){
-        if(!node) return 0;
-        return node->val+getts(node->left)+getts(node->right);
-    }
-    long getss(TreeNode* node) {
-        if(!node) return 0;
-        long left=getss(node->left);
-        long right=getss(node->right);
-        long subtree=node->val+left+right;
-        res=max(res,subtree*(ts-subtree));
-        return subtree;
-    }
-};
+  * struct TreeNode {
+   *     int val;
+    *     TreeNode *left;
+     *     TreeNode *right;
+      *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+       *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+         * };
+          */
+          class Solution {
+          public:
+              int acc(TreeNode* root) noexcept {
+                      if (!root) return 0;
+                              return root->val += acc(root->left) + acc(root->right);
+                                  }
+
+                                      int total = 0;
+                                          int mid = 0;
+
+                                              int dfs(TreeNode* node) noexcept {
+                                                      if (!node) return mid;
+                                                              int x = max(node->val, total - node->val) - mid;
+                                                                      return min(x, min(dfs(node->left), dfs(node->right)));
+                                                                          }
+
+                                                                              int maxProduct(TreeNode* root) noexcept {
+                                                                                      total = acc(root);
+                                                                                              mid = total / 2;
+                                                                                                      int x = dfs(root);
+                                                                                                              int a = x + mid, b = total - a;
+                                                                                                                      return (int64_t{a} * b) % 1'000'000'007u;
+                                                                                                                          }
+                                                                                                                          };
