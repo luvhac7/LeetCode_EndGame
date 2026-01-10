09801c350cc41c2aedb5b803d6684b41bbd03d1c
Luv <ks753159@gmail.com>
2026-01-10 12:15:19 +0530

    Time: 2811 ms (16.67%), Space: 742.9 MB (29.17%) - LeetHub

--- 3563-lexicographically-smallest-string-after-adjacent-removals/3563-lexicographically-smallest-string-after-adjacent-removals.cpp
+++ 3563-lexicographically-smallest-string-after-adjacent-removals/3563-lexicographically-smallest-string-after-adjacent-removals.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-typedef const int I;
+    bool solve(char a,char b)
+    {
+        return abs(a-b)==1 || abs(a-b)==25;
+    }
     string lexicographicallySmallestString(string s) {
-        I n=s.size();
+        int n=s.size();
         vector<vector<string>>dp(n+1,vector<string>(n+1));
         for(int d=1;d<=n;d++)
         {
             for(int i=0;i+d<=n;i++)
             {
-                I j=i+d;
-                string minstr=s[i]+dp[i+1][j];
+                int j=i+d;
+                string ans=s[i]+dp[i+1][j];
                 for(int k=i+1;k<j;k++)
                 {
-                    if(iscons(s[i],s[k])&& dp[i+1][k].empty())
+                    if(solve(s[i],s[k])&& dp[i+1][k].empty())
                     {
-                        minstr=min(minstr,dp[k+1][j]);
+                        ans=min(ans,dp[k+1][j]);
                     }
-                
-                    
-                }    dp[i][j]=minstr;
+                }
+                dp[i][j]=ans; 
             }
-
         }
-
-return dp[0][n];
-            }
-            bool iscons(char a,char b)
-            {
-                return abs(a-b) ==1|| abs(a-b)==25;
-            }
+        return dp[0][n];
+    }
 };
