bbadf184eff7e40737bf40c935d7b49cae648cff
Luv <ks753159@gmail.com>
2026-01-10 21:40:34 +0530

    Time: 0 ms (100%), Space: 10.2 MB (73.94%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    typedef const int ci;
+    int solve(vector<int>&a,int st,int end)
+    {
+        int p1=0,p2=0;
+        for(int i=st;i<=end;i++)
+        {
+            int ans=max(p2+a[i],p1);
+            p2=p1;
+            p1=ans;
+        }
+        return p1;
+    }
     int rob(vector<int>& a) {
-        if(a.empty()) return 0;
         int n=a.size();
-        if(n==1 && a[0]==0) return 0;
-        if(n==1 && a[0]==100) return 100;
-        if(n==1) return 1;
-        auto rob=[&](int l,int r){
-            int p1=0;
-            int p2=0;
-            for(int i=l;i<=r;i++){
-                ci dp=max(p1,p2+a[i]);
-                p2=p1;
-                p1=dp;
-            }
-            return p1;
-        };
-        return max(rob(0,n-2),rob(1,n-1));
+        if(n==1) return a[0];
+        return max(solve(a,1,n-1),solve(a,0,n-2));
     }
 };
