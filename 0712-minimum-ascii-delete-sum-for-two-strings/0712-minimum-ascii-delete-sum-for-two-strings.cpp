b4ebe80761fda9b43926001db1787653af2bc733
Luv <ks753159@gmail.com>
2026-01-10 10:46:09 +0530

    Time: 14 ms (78.67%), Space: 18.3 MB (23.97%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        int n=s1.size(),m=s2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<s1.size();i++)
+     int n=s1.size();
+     int m=s2.size();
+     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+     for(int i=0;i<n;i++)
+     {
+        for(int j=0;j<m;j++)
         {
-            for(int j=0;j<s2.size();j++)
             if(s1[i]==s2[j])
             {
                 dp[i+1][j+1]=dp[i][j]+s1[i];
             }
             else{
-                dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j]);
+                dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1]);
             }
         }
-        int ans=0;
-        for(char c:s1)ans+=c;
-        for(char c:s2)ans+=c;
-        return ans-2*dp[n][m];
+     }
+        int t=0;
+        for(char c:s1) t+=c;
+        for(char c:s2) t+=c;
+        return t-2*dp[n][m];
+        
     }
 };
