23719254f657dd77fe6fe37c8b9b63d9bf1a8743
Luv <ks753159@gmail.com>
2026-01-10 11:00:05 +0530

    Time: 12 ms (82.71%), Space: 18.2 MB (49.34%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -7,17 +7,19 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(s1[i]==s2[j]){
-                dp[i+1][j+1]=dp[i][j]+s1[i];
+                if(s1[i]==s2[j])
+                {
+                    dp[i+1][j+1]=dp[i][j]+s1[i];
                 }
                 else{
                     dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1]);
                 }
             }
+
         }
         int t=0;
         for(char c:s1)t+=c;
-        for(char c:s2)t+=c;
+        for(char c:s2) t+=c;
         return t-2*dp[n][m];
     }
 };
