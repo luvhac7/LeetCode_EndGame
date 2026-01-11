e364a5cdf62d153de487882b5a1c8d1418470524
Luv <ks753159@gmail.com>
2026-01-11 08:19:15 +0530

    Time: 0 ms (100%), Space: 18 MB (66.56%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    int area(vector<int>&a)
+int solve(vector<int>&a)
+{
+    int n=a.size();
+    stack<int>st;
+    a.push_back(0);
+    int ans=0;
+    for(int i=0;i<=n;i++)
     {
-        int n=a.size();
-        stack<int>st;
-        a.push_back(0);
-        int maxi=0;
-        for(int i=0;i<=n;i++)
+        while(!st.empty() && (a[st.top()]>a[i]))
         {
-            while(!st.empty() && (a[st.top()]>a[i]))
-            {
-                int h=a[st.top()];
-                st.pop();
-                int w=st.empty() ? i:i-st.top()-1;
-                maxi=max(maxi,h*w);
-            }
-            st.push(i);
+            int h=a[st.top()];
+            st.pop();
+            int w=st.empty() ? i:i-st.top()-1;
+            ans=max(ans,h*w);
         }
-        a.pop_back();
-        return maxi;
+        st.push(i);
     }
+    a.pop_back();
+    return ans;
+}
     int maximalRectangle(vector<vector<char>>& a) {
         int m=a.size();
         int n=a[0].size();
-        vector<int>h(n,0);
         int ans=0;
+        vector<int>h(n,0);
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(a[i][j]=='1')
+                if(a[i][j]=='1'){
                     h[j]+=1;
+                }
                 else{
                     h[j]=0;
                 }
             }
-            ans=max(ans,area(h));
+            ans=max(ans,solve(h));
+            
         }
         return ans;
     }
