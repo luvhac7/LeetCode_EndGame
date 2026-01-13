8d82236511d3a471fd06b06e2eca6a2f24f0981e
Luv <ks753159@gmail.com>
2026-01-13 12:39:51 +0530

    Time: 128 ms (39.02%), Space: 198.7 MB (52.02%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.cpp
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-typedef double d;
-    d area(vector<vector<int>>&sq)
-    {
-        d t=0;
-        for(auto &s:sq)
-        {
-            t+=1.0*s[2]*s[2];
-        }
-        return t;
-    }
-    d below(vector<vector<int>>&sq,d y)
-    {
-        d a=0.0;
-        for(auto &s:sq)
-        {
-            d btm=s[1];
-            d len=s[2];
-            if(y>btm){
-                a+=min(y-btm,len)*len;
+    typedef double d;
+    d below(vector<vector<int>>& sq, d y) {
+        d ta = 0;
+        for (auto& s : sq) {
+            d btm = s[1];
+            d len = s[2];
+            if (btm < y) {
+                ta += min(y - btm, len) * len;
             }
         }
-        return a;
+        return ta;
     }
-
+    d area(vector<vector<int>>& sq) {
+        d ta = 0.0;
+        for (auto& s : sq) {
+            ta += (d)s[2] * s[2];
+        }
+        return ta;
+    }
     double separateSquares(vector<vector<int>>& sq) {
-        d l=1e18,h=-1e18;
-        for(auto &s:sq)
-        {
-            l=min(l,(d)s[1]);
-            h=max(h,(d)s[1]+s[2]);
+        // bt
+        d h = -1e18, l = 1e18;
+        for (auto& s : sq) {
+            h = max(h, (d)s[1] + s[2]);
+            l = min(l, (d)s[1]);
         }
-        d t=area(sq);
-        for(int i=0;i<80;i++){
-            d mid=(l+h)/2;
-            if(below(sq,mid)*2<t){
+        d ta=area(sq);
+        for (int i = 0; i < 80; i++) {
+            d mid = (l + h) / 2;
+            if (below(sq, mid) * 2 < ta) {
                 l=mid;
-            }
-            else{
+            } else {    
                 h=mid;
             }
         }
