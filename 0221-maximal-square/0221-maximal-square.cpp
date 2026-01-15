74e26b62aaf18fd60b0c20ff0fb5f65751113bcd
Luv <ks753159@gmail.com>
2026-01-15 22:24:37 +0530

    Time: 12 ms (40.16%), Space: 30.9 MB (63.45%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        int mx = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    if (i == 0 || j == 0)
-                        dp[i][j] = 1;
-                    else
-                        dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1],
-                                            dp[i - 1][j - 1]});
-                    mx = max(mx, dp[i][j]);
+    int maximalSquare(vector<vector<char>>& a) {
+        int m=a.size(),n=a[0].size(),mx=0;
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(a[i][j]=='1'){
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=1;
                 }
+                
+                else{
+                    dp[i][j]=1+min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
+                }
+                
+                mx=max(mx,dp[i][j]);
             }
-        }
-        return mx * mx;
+            }
+    }
+        return mx*mx;
     }
 };
