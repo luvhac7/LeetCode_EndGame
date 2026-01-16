3e9f8b26d03a32832d9682ce9fb59082a345ef6f
Luv <ks753159@gmail.com>
2026-01-17 02:37:31 +0530

    Time: 217 ms (43.94%), Space: 86.7 MB (41.79%) - LeetHub

--- 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int centeredSubarrays(vector<int>& a) {
-        int x=0;
+        int ans=0;
         for(int i=0;i<a.size();i++)
         {
-            int y=0;
+            int sum=0;
             unordered_set<int>st;
             for(int j=i;j<a.size();j++)
             {
-                y+=a[j];
+                sum+=a[j];
                 st.insert(a[j]);
-                x+=st.count(y)==true ? 1:0;
+                ans+=st.count(sum)==true ? 1:0;
             }
         }
-        return x;
+        return ans;
     }
 };
