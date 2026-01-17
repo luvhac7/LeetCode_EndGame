baf85271b8da02b3a4f628de858a1ee31f38760a
Luv <ks753159@gmail.com>
2026-01-17 09:59:50 +0530

    Time: 119 ms (30.17%), Space: 59.3 MB (80.17%) - LeetHub

--- 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
 typedef long long ll;
-    long long largestSquareArea(vector<vector<int>>& bl, vector<vector<int>>& tr) {
-        ll maxi=0,s=0;
-  for(int i=0;i<bl.size();i++)
-  {
-    for(int j=i+1;j<bl.size();j++)
-    {
-        int x1=max(bl[i][0],bl[j][0]);
-        int y1=max(bl[i][1],bl[j][1]);
-        int x2=min(tr[i][0],tr[j][0]);
-        int y2=min(tr[i][1],tr[j][1]);
-
-        ll w=x2-x1;
-        ll h=y2-y1;
-if(w>0&&h>0){
-        ll ans=min(w,h);
-        maxi=max(ans,maxi);
-    }
-    }
-  }      
-  return maxi*maxi;
+ll x=0;
+    long long largestSquareArea(vector<vector<int>>& b, vector<vector<int>>& t) {
+        for(int i=0;i<b.size();i++){for(int j=i+1;j<b.size();j++){
+            int x1=max(b[i][0],b[j][0]),
+            y1=max(b[i][1],b[j][1]),
+            x2=min(t[i][0],t[j][0]),
+            y2=min(t[i][1],t[j][1]);
+            ll w=x2-x1,h=y2-y1;
+            if(w>0&&h>0){
+                ll mini=min(w,h);
+                x=max(x,mini);
+            }
+        }}
+        return pow(x,2);
     }
 };
