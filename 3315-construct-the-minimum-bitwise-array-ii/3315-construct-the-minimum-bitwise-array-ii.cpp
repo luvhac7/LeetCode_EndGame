bae05aa925d0d791618c410b562b6ded9f78203f
Luv <ks753159@gmail.com>
2026-01-21 07:35:07 +0530

    Time: 0 ms (100%), Space: 28.2 MB (61.17%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
 vector<int>res;
-    vector<int> minBitwiseArray(vector<int>& a) {
-        for(int i:a){
-            if(i!=2){
-                res.push_back(i-((i+1)&(-i-1))/2);
-            }
-            else res.push_back(-1);
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        for(int i:nums){
+            if(i!=2) res.push_back(i-((i+1)&(-i-1))/2);
+        
+        else res.push_back(-1);
         }
         return res;
     }
