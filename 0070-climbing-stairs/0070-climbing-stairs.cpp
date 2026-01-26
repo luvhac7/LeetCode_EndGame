764a9350a264e495d3fe8cab08f1a46c6bb298cc
Luv <ks753159@gmail.com>
2026-01-26 23:38:21 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.41%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n){
+        int a=1,b=1;
+        for(int i=2;i<=n;i++)
+        {
+            int sum=a+b;
+            b=a;
+            a=sum;
+        }
+        return a;
+    }
+    int climbStairs(int n) {
+        // if(n<3) return n;
+        // return climbStairs(n-1)+climbStairs(n-2);
+        return fib(n);
+    }
+};
