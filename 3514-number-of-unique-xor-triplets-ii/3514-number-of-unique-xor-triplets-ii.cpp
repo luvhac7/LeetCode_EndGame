e1aac7d7bade588a0ea3d39653149eddada3b2c7
Luv <ks753159@gmail.com>
2026-01-28 02:21:00 +0530

    Time: 2026 ms (64%), Space: 98.4 MB (50%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& a) {
-    unordered_set<int>ans,res;
-    int n=a.size();
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i;j<n;j++)
+        int n=a.size();
+        unordered_set<int>st;
+        for(int i=0;i<n;i++)
         {
-            ans.insert(a[i]^a[j]);
+            for(int j=i;j<n;j++)
+            {
+                st.insert(a[i]^a[j]);
+            }
         }
-    }
-    for(auto i:ans)
-    {
-        for(auto j:a)
+        unordered_set<int>res;
+        for(auto i:st)
         {
-            res.insert(i^j);
+            for(auto j:a)
+            {
+                res.insert(i^j);
+            }
         }
-    }
-    return res.size();
+        return res.size();
     }
 };
