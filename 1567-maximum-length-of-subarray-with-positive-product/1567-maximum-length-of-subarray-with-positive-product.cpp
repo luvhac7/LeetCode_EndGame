1a3bd7879e056701a10ac8df612db0a357985d9a
Luv <ks753159@gmail.com>
2026-02-01 21:03:51 +0530

    Time: 0 ms (100%), Space: 61.7 MB (61.04%) - LeetHub

---
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getMaxLen(vector<int>& a) {
+        int pos=0,neg=0;
+        int ans=0;
+        for(int i:a)
+        {
+            if(i==0)
+            {
+                pos=neg=0;
+            }
+            else if(i>0){
+                pos++;neg=neg==0?0:neg+1;
+            }
+            else{
+                int temp=pos;
+                pos=neg==0 ? 0:neg+1;
+                neg=temp+1;
+            }
+            ans=max(ans,pos);
+        }
+        return ans;
+    }
+};
