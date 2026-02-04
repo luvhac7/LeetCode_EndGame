7bcf7294088532f9d3415b2c64ce447a9eab75fa
Luv <ks753159@gmail.com>
2026-02-04 13:00:05 +0530

    Time: 0 ms (100%), Space: 143.5 MB (66.36%) - LeetHub

---
+++ 3640-trionic-array-ii/3640-trionic-array-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+typedef long long ll;
+    ll maxSumTrionic(vector<int>& a) {
+        a.push_back(INT_MIN);
+        int n = a.size();
+
+        vector<ll> pref(n + 1, 0);
+        for (int i = 0; i < n; i++)
+            pref[i + 1] = pref[i] + a[i];
+
+        ll ans = LLONG_MIN;
+        int pre = -1, cur = -1;
+        bool inc = false;
+
+        for (int i = 0; i < n - 1; i++) {
+            int x = a[i], y = a[i + 1];
+
+            if (x < y) { // increasing
+                if (!inc) {
+                    inc = true;
+                    cur = i;
+                }
+                if (pre != -1)
+                    ans = max(ans, pref[i + 2] - pref[pre]);
+            } else if (y < x) { // decreasing
+                if (inc)
+                    pre = cur;
+                inc = false;
+            } else { // equal
+                pre = cur = -1;
+                inc = false;
+            }
+
+            while (pre != -1 && pre + 2 < n && a[pre + 1] < a[pre + 2] &&
+                   a[pre] < 0) {
+                pre++;
+            }
+        }
+        return ans;
+    }
+};
