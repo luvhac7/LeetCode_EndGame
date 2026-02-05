9e14803417e8589380801c8f6ce784e04206bb60
Luv <ks753159@gmail.com>
2026-02-06 01:56:57 +0530

    Time: 2 ms (61.78%), Space: 12.1 MB (11.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool canConstruct(string r, string m) {
-        // if(r==m) return true;
-        // sort(r.begin(),r.end());sort(m.begin(),m.end());
-        // int n=min(r.size(),m.size());
-        // // for(int i=0;i<n;i++)
-        // // {
-        // //     // if(s.substr())
-        // // }
-vector<int>f(26);
-for(int i=0;i<m.size();i++) f[m[i]-'a']++;
-for(int i=0;i<r.size();i++){
-    f[r[i]-'a']--;
-}
-    for(int i=0;i<26;i++)
-    {
-        if(f[i]<0) return false;
+// vector<int>f(26);
+unordered_map<char,int>mp;
+// for(int i=0;i<m.size();i++) f[m[i]-'a']++;
+for(char c:m) mp[c]++;
+for(char c:r) mp[c]--; 
+// for(int i=0;i<r.size();i++){
+//     f[r[i]-'a']--;
+// }
+    // for(int i=0;i<26;i++)
+    // {
+    //     if(f[i]<0) return false;
+    // }
+    for(auto &i:mp){
+        if(i.second<0) return false;
     }
     return true;
     }
