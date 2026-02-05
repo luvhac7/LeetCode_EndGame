a720cfcda768f54dab2fb988c79b999e5149fc5c
Luv <ks753159@gmail.com>
2026-02-05 10:03:56 +0530

    Time: 0 ms (100%), Space: 25.8 MB (39.55%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    vector<int> constructTransformedArray(vector<int>&a) {
-        int n=a.size();vector<int>res(n);
-        for(int i=0;i<n;i++) res[i]=a[(i+(a[i])%n+n)%n];
+    vector<int> constructTransformedArray(vector<int>&a) {int n=a.size();
+        vector<int>res(n);for(int i=0;i<n;i++)res[i]=a[(i+(a[i])%n+n)%n];
         return res;
     }
 };
