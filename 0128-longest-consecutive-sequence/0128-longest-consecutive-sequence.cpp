8a2b05a3a0cb61a6d4f666501210408105f00419
Luv <ks753159@gmail.com>
2026-02-09 20:06:34 +0530

    Time: 99 ms (17.76%), Space: 89 MB (23.66%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& a) {
-        int ans=1,cnt=1;
-        int n=a.size();
-        if(a.empty()) return 0;
-        // int mini=*min_element(a.begin(),a.end());
-        // int maxi=*max_element(a.begin(),a.end());
-        sort(a.begin(),a.end());
-        for(int i=1;i<a.size();i++)
+        // int ans=1,cnt=1;
+        // int n=a.size();
+        // if(a.empty()) return 0;
+        // // int mini=*min_element(a.begin(),a.end());
+        // // int maxi=*max_element(a.begin(),a.end());
+        // sort(a.begin(),a.end());
+        // for(int i=1;i<a.size();i++)
+        // {
+        //     if(a[i]==a[i-1]) continue;
+        //     if(a[i]==a[i-1]+1)
+        //     {
+        //         cnt++;
+        //     }
+        //     else{
+        //         ans=max(ans,cnt);
+        //         cnt=1;
+        //     }
+        // }
+        // return max(cnt,ans);
+
+//--------------------O(N)-----------------------------
+unordered_set<int>st;for(int i:a) st.insert(i);
+int y=0;
+for(int i:a)
+{
+    if(st.find(i)!=st.end())
+    {
+        if(st.find(i-1)==st.end())
         {
-            if(a[i]==a[i-1]) continue;
-            if(a[i]==a[i-1]+1)
-            {
-                cnt++;
-            }
-            else{
-                ans=max(ans,cnt);
-                cnt=1;
-            }
+            int cur=i,x=0;
+            while(st.find(cur)!=st.end()){
+            st.erase(cur);
+         //   i++;
+           cur++;
+            x++;
+         //   y++;
         }
-        return max(cnt,ans);
+    
+        y=max(y,x);
+    }
+    }
+}
+return y;
+
+
+
+
+
     }
 };
