0187aac0aca3c5d6c2a7be304ea27f0bf68eb4f9
Luv <ks753159@gmail.com>
2026-02-11 13:55:14 +0530

    Time: 2398 ms (6.42%), Space: 472.8 MB (76.19%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+typedef long long ll;
+int x=0;
     int longestBalanced(vector<int>& a) {
-        int n=a.size(),maxi=0;
-        for(int i=0;i<a.size();i++)
+        int n=a.size();
+        int res=0;
+        for(int i=0;i<n;i++)
+         {
+        //     unordered_set<int>o,e;
+        //     for(int j=i;j<n;j++)
+        //     {
+        //         if(a[j]&1) o.insert(a[j]);
+        //         else e.insert(a[j]);
+        //         if(o.size()==e.size()) res=max(res,j-i+1);
+        //     }
+        set<int>st;int o=0,e=0;
+        for(int j=i;j<n;j++)
         {
-            unordered_set<int>o,e;
-            for(int j=i;j<a.size();j++)
+            if(!st.count(a[j]))
             {
-                if(a[j]&1) o.insert(a[j]);
-                else e.insert(a[j]);
-                if(o.size()==e.size()) maxi=max(maxi,j-i+1);
+                if(a[j]%2==0) e++;
+                else o++;
+                st.insert(a[j]);
             }
+            if(o==e) x=max(x,j-i+1);
         }
-        return maxi;
+        }
+        return x;
     }
 };
