c9972d2bcba72e8125a129f642930b21d12bdd1e
Luv <ks753159@gmail.com>
2026-02-14 18:44:49 +0530

    Time: 4 ms (87.44%), Space: 23 MB (45.72%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,15 +1,42 @@
 class Solution {
 public:
-typedef long long ll;
-bool solve(vector<int>&a,int m,int k)
-{
-    ll ts=0;
-    for(int  i=0;i<a.size();i++)
-    {
-        ts+=(a[i]+m-1)/m;
-    }
-    return ts<=k;
+/*
+inti:
+pile>=max_element
+1:[5,10,3],k=4
+->take:a[0]=5->pile 
+->10%pile=2->cnt
+->3>5 so cnt=1
+check pile>a[i] then cnt=1
+else cnt+=a[i]%pile
 
+*/
+//---------------Brute-force---(speed* j)----------------
+ typedef long long ll;
+// int solve(vector<int>&a,int h)
+// {
+//     int maxi=*max_element(a.begin(),a.end());
+//    for(int speed=1;speed<=maxi;seed++)
+//    {
+//     ll ans=0;
+//     for(int j=0;j<a.size();j++)
+//     {
+//         ans+=(a[j]+speed-1)/speed;
+//     }
+//     if(ans<=h) return speed;
+//    }
+//    return maxi;
+// }
+
+//-----------Optimal
+bool ok(vector<int>&a,int h,int m)
+{
+ll res=0;
+for(int i=0;i<a.size();i++)
+{
+res+=(a[i]+m-1)/m;
+}
+return res<=h;
 }
 int bs(vector<int>&a,int k)
 {
@@ -18,11 +45,9 @@ int bs(vector<int>&a,int k)
     while(l<=h)
     {
         int m=l+(h-l)/2;
-        if(solve(a,m,k))
-
+        if(ok(a,k,m))
         {
-            res=m;
-            h=m-1;
+            res=m;h=m-1;
         }
         else{
             l=m+1;
@@ -31,6 +56,7 @@ int bs(vector<int>&a,int k)
     return res;
 }
     int minEatingSpeed(vector<int>& a, int h) {
-        return bs(a,h);
+       // return solve(a,h);
+       return bs(a,h);
     }
 };
