9891035e39b7cdf6bcb3767d9dc3bc62241e893a
Luv <ks753159@gmail.com>
2026-02-16 09:01:55 +0530

    Time: 0 ms (100%), Space: 8.4 MB (9.32%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int reverseBits(int n) {
-
-       int ans=0;
-       for(int i=0;i<32;i++)
-       {
-        ans=(ans<<1)|(n&1);
-        n>>=1;
-       }  
-       return ans;
-        }
+    bitset<32>bin(n);
+    string x=bin.to_string();
+    reverse(x.begin(),x.end());
+    return (int)bitset<32>(x).to_ulong();
+    }
 };
