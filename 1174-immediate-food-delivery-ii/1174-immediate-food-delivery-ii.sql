c0bb321e0cad70d25658edef9ba1eb5752c943d6
Luv <ks753159@gmail.com>
2026-02-19 22:30:09 +0530

    Time: 786 ms (13.75%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,7 +1,7 @@
-select round(100*sum(order_date=customer_pref_delivery_date)/count(*),2) as immediate_percentage
-from delivery
-where (customer_id,order_date) in(
-select customer_id,min(order_date)
-from delivery
-group by customer_id
-);
+#optmized soln
+select round(sum(case when date(order_date)=date(customer_pref_delivery_date)then 1 else 0 end)*100/count(*),2) immediate_percentage
+from (
+    select *,row_number() over(partition by customer_id order by order_date) as rn
+    from delivery
+)t
+where rn=1
