8b8f5b89578750d08a72301cf1cac210eba01d71
Luv <ks753159@gmail.com>
2026-02-21 23:47:58 +0530

    Time: 311 ms (89.98%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,8 @@
+# case bna ke 
+# check kana h ki id=(max(d)from sear)
+select case when id=(select max(id) from seat) and id%2=1 then id when id%2=1 then id+1
+else id-1
+end as id,
+student 
+from seat
+order by id asc
