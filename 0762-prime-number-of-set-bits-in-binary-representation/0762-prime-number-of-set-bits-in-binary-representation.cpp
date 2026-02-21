c52b9b538e105d41181bc23db416f779199bc3f6
Luv <ks753159@gmail.com>
2026-02-21 17:47:11 +0530

    Time: 3 ms (72.17%), Space: 8 MB (61.81%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -3,14 +3,17 @@ public:
 vector<int>b={2,3,5,7,11,13,17,19};
     int countPrimeSetBits(int l, int r) {
         int cnt = 0;
-        
         for(int i = l; i <= r; i++) {
+            int bit=__builtin_popcount(i);
            for(int j=0;j<b.size();j++)
            {
-            if(__builtin_popcount(i)==b[j]) cnt++;
+            if(bit==b[j])
+            {
+                cnt++;
+                break;
+            }
            }
         }
-        
         return cnt;
     }
 };
