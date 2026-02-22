5ace3fd280cf02493cb1bca28db3b37a04bc72e9
Luv <ks753159@gmail.com>
2026-02-22 11:57:04 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.49%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        bitset<32>b(n);
-        string s=b.to_string();
-        int ans=0,last=-1;
-        for(int i=0;i<32;i++)
+     bitset<32>b(n);
+     string s=b.to_string();
+     int x=0,r=-1;
+     for(int i=0;i<32;i++)
+     {
+        if((n>>i)&1)
         {
-            if((n>>i)&1)
+            if(r!=-1)
             {
-                if(last!=-1)
-                {
-                    ans=max(ans,i-last);
-                }
-                last=i;
+                x=max(x,i-r);
             }
+            r=i;
         }
-        return ans;
+     }   
+     return x;
     }
 };
