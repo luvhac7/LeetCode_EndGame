382615d6a46f6d088a2fa6017e7ff81d4bcec27b
Luv <ks753159@gmail.com>
2026-02-24 19:22:15 +0530

    Time: 538 ms (56.74%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,17 +1,14 @@
 # Write your MySQL query statement below
-#select distinct a.num as ConsecutiveNums from logs a
-#join logs b on a.id=b.id+1 and a.num=b.num
-#join logs c on a.id=c.id+2 and a.num=c.num
--- join logs d on a.id=d.id+3 and a.num=d.num
-
-select distinct num as consecutivenums 
-from (
-    select 
-        num,
-        lag(num,1) over(order by id) as p1,
-        lag(num,2)over (order by id) as p2
-        from logs
-)t
-where num=p1
-and
-num=p2
+# return 3 time ->no that occ
+#sel distinct no from that table 
+# then we use wf -> lag()
+#check end num=n1 and num =n2
+#simify:
+select distinct num as ConsecutiveNums
+from(
+    select num,
+    lag(num) over(order by id) as n1,
+    lead(num) over(order by id) as n2
+    from logs
+)x
+where num=n1 and num=n2;
