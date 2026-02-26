3304aba49eda0a60234f81ce608d66d1bd022ea4
Luv <ks753159@gmail.com>
2026-02-26 17:34:08 +0530

    Time: 615 ms (76.57%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+select d.name as department,e.name as employee,e.salary   
+from 
+(
+    select *,
+    dense_rank()oveR(partition by departmentid order by salary desc) rnk
+    from employee
+)e
+join department d on e.departmentid=d.id
+where rnk=1;
