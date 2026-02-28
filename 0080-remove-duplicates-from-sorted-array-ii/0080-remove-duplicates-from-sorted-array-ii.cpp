ecc84b50ec34ee4066e8396c8d37d85764c2ebe4
Luv <ks753159@gmail.com>
2026-02-28 19:05:38 +0530

    Time: 11 ms (32.07%), Space: 20.9 MB (5.25%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
 
+int un(vector<int>&a)
+{
+unordered_map<int,int>f;
+int k=0;
+for(int i:a)
+{
+    f[i]++;
+    if(f[i]<=2)
+    {
+        a[k]=i;
+        k++;
+    }
+}
+return k;
+}
 void f(vector<int>&a,int &i,int &j,int cnt)
 {
     int k=0,val=a[j];
@@ -11,12 +26,13 @@ void f(vector<int>&a,int &i,int &j,int cnt)
 }
     int removeDuplicates(vector<int>& a) {
         int i=0,sz=a.size();
-        for(int j=0;j<a.size();)
-        {
-            f(a,i,j,2);
-            while(j<a.size()&&a[j]==a[j-1]) j++;
-        }
-        return i;
+        // for(int j=0;j<a.size();)
+        // {
+        //     f(a,i,j,2);
+        //     cout<<a[i]<<" "<<a[j]<<" "<<"\n"; 
+        //     while(j<a.size()&&a[j]==a[j-1]) j++;
+        // }
+    return un(a);
         
     }
 };
