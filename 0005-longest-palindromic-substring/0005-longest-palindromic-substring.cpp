8cfcd3b88bb45379d8f2a23c2d448cfcd9f39b5e
Luv <ks753159@gmail.com>
2026-02-28 16:14:19 +0530

    Time: 5 ms (91.04%), Space: 9.4 MB (79.6%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-
-    int ex(string s,int l,int r)
-    {
-        while(l>=0 && r<s.size() && s[l]==s[r])
-        {
-            l--;r++;
-        }
-        return r-l-1;
-    }
     string longestPalindrome(string s) {
-        if(s.empty()) return " ";
-       int len=0,end=0,n=s.size(),st=0;
-    for(int i=0;i<n;i++)
-    {
-//"babad"
-//odd->a a
-//even->b b d
-//len=3
-//3>0->i=0 ->
-        int odd=ex(s,i,i);//
-        int even=ex(s,i,i+1);
-        int len=max(odd,even);
-        if(len>end-st)
+        int n=s.size(),st=0,len=1;
+        for(int i=0;i<n;i++)
         {
-            st=i-(len-1)/2;//->i=0-(3-1)/2=1
-            end=i+(len/2);//->i0+(3/2)=1.5=2//1->st,end->()
+            //odd
+            int l=i,h=i;
+            while(l>=0 && h<n && s[l]==s[h])
+            {
+                if(h-l+1>len)
+                {
+                    len=h-l+1;
+                    st=l;
+                }
+                h++;
+                l--;
+            }
+              l=i,h=i+1;
+             while(l>=0&&h<n&&s[l]==s[h])
+             {
+                if(h-l+1>len)
+                {
+                    len=h-l+1;
+                    st=l;
+                }
+                l--;h++;
+             }
         }
+        return s.substr(st,len);
     }
-    return s.substr(st,end-st+1);
-
-        }
 };
