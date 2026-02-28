9fb9f49b07e8edf0d666722f761aba3b545bf9d7
Luv <ks753159@gmail.com>
2026-02-28 19:21:03 +0530

    Time: 1 ms (74.57%), Space: 8.1 MB (96.96%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        int n[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string rom[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+int x[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    string p[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    string intToRoman(int a) {
         string res;
         for(int i=0;i<13;i++)
         {
-            while(num>=n[i])
+            while(a>=x[i])
             {
-                res.append(rom[i]);
-                num-=n[i];
+                res.append(p[i]);
+                a-=x[i];
             }
         }
-        return res;
+        return res; 
     }
 };
