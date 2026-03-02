a1f199a4421239c4857d97df1702a33065a6c772
Luv <ks753159@gmail.com>
2026-03-02 21:03:07 +0530

    Time: 8 ms (7.32%), Space: 32.6 MB (6.1%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    int minSwaps(vector<vector<int>>& g) {
-        int n=g.size();
-        vector<int>z(n);for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=n-1;j>=0&&g[i][j]==0;j--)
-            {
-                cnt++;
-            }
-            z[i]=cnt;
+    int minSwaps(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int ans = 0;
+        vector<int>tz;
+        for(auto v : grid){
+            int cnt = 0;
+            for(int j = v.size()-1;j>=0 && v[j] == 0;j--) cnt++;
+            tz.push_back(cnt);
         }
-        int sw=0;
-        for(int i=0;i<n;i++)
-        {
-            int need=n-i-1;
-            int f=-1;
-            for(int j=i;j<n;j++)
-            {
-                if(z[j]>=need)
-                {
-                    f=j;
-                    break;
-                }
-            }
-            if(f==-1)
-            {
-                return -1;
-            }
-            for(int j=f;j>i;j--)
-            {
-                swap(z[j],z[j-1]);
-                sw++;
+
+        for(int i=0;i<n;i++){
+            int need = n - i -1;
+            int j = i;
+            while(j < n && tz[j] < need) j++;
+
+            if(j == n) return -1;
+
+            while(j > i){
+                ans++;
+                swap(tz[j],tz[j-1]);
+                j--;
             }
         }
-        return sw;
+        return ans;
     }
 };
