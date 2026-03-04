00b58260f73b5002066fb3844c1b75e3111b7ea2
Luv <ks753159@gmail.com>
2026-03-04 19:12:38 +0530

    Time: 3 ms (70.27%), Space: 16.4 MB (16.92%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-string res;
-void dfs(int op,int cp,string s,int n,vector<string>&res)
+void solve(string op,int st,int end,vector<string>&res)
 {
-    if(op==cp && op+cp==n*2){
-        res.push_back(s);
-        return;
+    if(st==0 && end==0){
+        res.push_back(op);return;
     }
-    if(op<n)
-    {
-        dfs(op+1,cp,s+"(",n,res);
+    if(st==end){
+        string op1=op;
+        op1.push_back('(');
+        solve(op1,st-1,end,res);
     }
-    if(cp<op){
-        dfs(op,cp+1,s+")",n,res);
+    else if(st==0){
+        string op1=op;
+        op1.push_back(')');
+        solve(op1,st,end-1,res);
+    }
+    else if(end==0){
+        string op2=op;
+        op2.push_back('(');
+        solve(op2,st-1,end,res);
+    }
+    else{
+        string op1=op,op2=op;
+        op1.push_back('(');op2.push_back(')');
+        solve(op1,st-1,end,res);
+        solve(op2,st,end-1,res);
     }
 }
     vector<string> generateParenthesis(int n) {
+        int st=n,end=n;
         vector<string>res;
-        dfs(0,0,"",n,res);
+        string op="";
+        solve(op,st,end,res);
         return res;
     }
 };
