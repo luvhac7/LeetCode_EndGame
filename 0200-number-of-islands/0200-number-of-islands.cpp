0b9500a495a8f68dca62be73378187e925f71bc1
Luv <ks753159@gmail.com>
2026-03-04 10:18:15 +0530

    Time: 25 ms (72.16%), Space: 16.3 MB (78.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>&v,int r,int c)
-    {
-        int R=v.size();
-        int C=v[0].size();
-        if(r<0 || c<0|| r>=R || c>=C ||v[r][c]=='0')
+void dfs(int i,int j,vector<vector<char>>&a)
+{
+    int n=a.size(),m=a[0].size();
+    if(i<0||j<0||i>=n||j>=m||a[i][j]=='0') return;
+    a[i][j]='0';
+    dfs(i-1,j,a);
+    dfs(i+1,j,a);
+    dfs(i,j-1,a);
+    dfs(i,j+1,a);
+}
+    int numIslands(vector<vector<char>>& a) {
+        int is=0;
+        for(int i=0;i<a.size();i++)
         {
-            return;
-        }
-        v[r][c]='0';
-        dfs(v,r+1,c);
-        dfs(v,r-1,c);
-        dfs(v,r,c+1);
-        dfs(v,r,c-1);
-    }
-    int numIslands(vector<vector<char>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        int min_zero=0;
-       if(mat.empty() || mat[0].empty()){
-        return 0;
-       }
-       for(int i=0;i<n;i++)
-       {
-        for(int j=0;j<m;j++)
-        {
-            if(mat[i][j]=='1')
+            for(int j=0;j<a[0].size();j++)
             {
-                min_zero++;
-                dfs(mat,i,j);
-            } 
+                if(a[i][j]=='1')
+                {
+                    dfs(i,j,a);
+                    is++;
+                }
+            }
         }
-       }
-       return min_zero;
+        return is;
     }
 };
