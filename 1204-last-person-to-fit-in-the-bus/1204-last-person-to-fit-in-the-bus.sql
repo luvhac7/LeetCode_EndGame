b009c83e0f1d170e8b5aa4563fff007457246185
Luv <ks753159@gmail.com>
2026-03-05 14:18:27 +0530

    Time: 920 ms (33.72%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,9 +1,7 @@
-with cte as(
-    select person_name,turn,
-    sum(weight) over(order by turn) as tw
+select person_name from(
+    select person_name,sum(weight) oveR(order by turn)as tw
     from queue
-)
-select person_name from cte
+)as cs
 where tw<=1000
 order by tw desc
-limit 1
+limit 1;
