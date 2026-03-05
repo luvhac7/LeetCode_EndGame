3467a6e3088cae8bee88913ec035f5ab786f3824
Luv <ks753159@gmail.com>
2026-03-05 13:59:19 +0530

    Time: 606 ms (21.13%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,14 +1,8 @@
-# Write your MySQL query statement below
-# return 3 time ->no that occ
-#sel distinct no from that table 
-# then we use wf -> lag()
-#check end num=n1 and num =n2
-#simify:
-select distinct num as ConsecutiveNums
+select distinct num as ConsecutiveNums 
 from(
     select num,
-    lag(num) over(order by id) as n1,
-    lead(num) over(order by id) as n2
+    lag(num) over(order by id) as p,
+    lead(num)over(order by id) as n
     from logs
-)x
-where num=n1 and num=n2;
+)z
+where num=p and num=n;
