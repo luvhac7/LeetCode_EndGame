128f4e56d97d97c8178ec3dc8e433f5e510ed0bd
Luv <ks753159@gmail.com>
2026-03-08 18:12:19 +0530

    Time: 43 ms (59.95%), Space: 57.5 MB (56.32%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-bool palind(string s)
+bool is(string s)
 {
-    int l=0,h=s.size()-1;
-    while(l<h){
-        if(s[l++]!=s[h--]) return false;
+int i=0,j=s.size()-1;
+while(i<j)
+{
+    if(s[i++]!=s[j--]) return false;
+}
+return true;
+}
+void dfs(string s,vector<vector<string>>&res,vector<string>&cur,int idx)
+{
+    if(idx==s.size()){
+        res.push_back(cur);return;
     }
-    return true;
-}
-void back(string s,int idx,vector<vector<string>>&ans,vector<string>&cur)
-{
-if(idx==s.size()){
-    ans.push_back(cur);
-    return ;
-}
-string x="";
-for(int i=idx;i<s.size();i++)
-{
-    x+=s[i];
-    if(palind(x))
+    string x="";
+    for(int i=idx;i<s.size();i++)
     {
-        cur.push_back(x);
-        back(s,i+1,ans,cur);
-        cur.pop_back();
+        x+=s[i];
+        if(is(x))
+        {
+            cur.push_back(x);
+            dfs(s,res,cur,i+1);
+            cur.pop_back();
+        }
     }
-}
 }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string>cur;
-        back(s,0,ans,cur);
-        return ans;
+        vector<string>a;
+        vector<vector<string>>b;
+        dfs(s,b,a,0);
+        return b;
     }
-}; 
+};
