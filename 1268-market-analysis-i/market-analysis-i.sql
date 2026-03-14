e8423b55edc6cc32e580f38b57bbe00637504f28
Luv <ks753159@gmail.com>
2026-03-14 17:23:27 +0530

    Time: 1591 ms (8.92%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,5 +1,8 @@
-select u.user_id as buyer_id,u.join_date,count(o.order_id) as orders_in_2019
+select u.user_id as buyer_id,u.join_date,
+(select count(*) from orders o2
+where o2.buyer_id=u.user_id
+and year(ordeR_Date)="2019"
+)
+as orders_in_2019
 from users u
-left join orders o on u.user_id=o.buyer_id
-and year(o.ordeR_date)=2019
-group by u.user_id,u.join_date;
+group by u.user_id
