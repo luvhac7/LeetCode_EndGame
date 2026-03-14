3783af14b1e0da0e314a1470bc1cca4a8f35c270
Luv <ks753159@gmail.com>
2026-03-14 19:52:46 +0530

    Time: 374 ms (9.26%) | Memory: 0B (100.00%) - LeetSync

--- 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
+++ 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
@@ -7,7 +7,7 @@ with cte1 as(
 	cte2 as(
 	select user_id,reaction,num,
 	sum(num)over(partition by user_id)as content_reactions,
-	round(num*1.0/sum(num) over(partition by useR_id),2) as reaction_ratio,
+	round(num/sum(num) over(partition by useR_id),2) as reaction_ratio,
 	row_number() over(partition by user_id order by num desc)as rnk
 	from cte1)
 	select user_id,
