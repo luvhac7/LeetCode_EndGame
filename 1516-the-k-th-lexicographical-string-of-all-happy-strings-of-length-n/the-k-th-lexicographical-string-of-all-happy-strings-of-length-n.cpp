f4c1a6c63873c11d665ef62ae1ea9aa5d5774ebf
Luv <ks753159@gmail.com>
2026-03-14 17:25:56 +0530

    Time: 86 ms (5.73%) | Memory: 26.7 MB (10.90%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    string getHappyString(int n, int k) {
-        if(k>(3<<(n-1))) return "";
-        queue<string> q;
-        q.push("");
-        while(k){
-            string temp=q.front();
-            q.pop();
-            for(char i='a';i<='c';i++){
-                if(temp.empty() || temp.back()!=i){
-                    q.push(temp+i);
-                    if(temp.size()+1==n) k--;
-                }
-                if(k==0) break;
-            }
+    set<string>st;
+    void solve(int n,int k,string s,int i)
+    {
+        if(i==n)
+        {
+            st.insert(s);   
+            return;
         }
-        return q.back();
+        if(i>=n) return;
+
+        for(char c='a';c<='c';c++)
+        {
+            if (!s.empty() && s.back() == c) continue;
+            s.push_back(c);
+            // if(st.count(s)==0)
+            // {
+                solve(n,k,s,i+1);
+                s.pop_back();
+            // }
+        }
+    }
+    string getHappyString(int n, int k) {
+        
+        solve(n,k,"",0);
+        if (k > st.size()) return "";
+        auto it=st.begin();
+        advance(it,k-1);
+        return *it;
     }
 };
