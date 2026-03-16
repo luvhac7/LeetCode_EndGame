925f44662a7b7fe1f5eea106b5752379a1c50a00
Luv <ks753159@gmail.com>
2026-03-16 15:37:18 +0530

    Time: 399 ms (5.51%) | Memory: 100.1 MB (5.52%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,59 +1,130 @@
 class Solution {
 public:
     vector<int> getBiggestThree(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        set<int> st;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>d1(n,vector<int>(m));
+        vector<vector<int>>d2(n,vector<int>(m));
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int j=0;j<m;j++){
+            int row = 0;
+            int col = j;
+            d1[row][col] = grid[row][col]; 
+            row++;
+            col--;
+            while(col>=0 && row<n){
+                d1[row][col] = d1[row-1][col+1] + grid[row][col];
+                row++;
+                col--;
+            }
+        }
+        for(int i=1;i<n;i++){
+            int row = i;
+            int col = m-1;
+            d1[row][col] = grid[row][col];
+            row++;
+            col--;
+            while(col>=0 && row<n){
+                d1[row][col] = d1[row-1][col+1] + grid[row][col];
+                row++;
+                col--;
+            }
+        }
 
-                // size 0 rhombus
-                st.insert(grid[i][j]);
+        for(int j=0;j<m;j++){
+            int row = 0;
+            int col = j;
+            d2[row][col] = grid[row][col];
+            row++;
+            col++;
+            while(col<m && row<n){
+                d2[row][col] = d2[row-1][col-1] + grid[row][col];
+                row++;
+                col++;
+            }
+        }
+        for(int i=1;i<n;i++){
+            int row = i;
+            int col = 0;
+            d2[row][col] = grid[row][col];
+            row++;
+            col++;
+            while(col<m && row<n){
+                d2[row][col] = d2[row-1][col-1] + grid[row][col];
+                row++;
+                col++;
+            }
+        }
 
-                for(int k = 1; ; k++) {
+        priority_queue<int,vector<int>,greater<int>> pq;
 
-                    int r = i + 2*k;
-                    int left = j - k;
-                    int right = j + k;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int ind1 = i;
+                int ind2 = j;
 
-                    if(r >= m || left < 0 || right >= n) break;
+                int side = 1;
+                while(true){
+                    int p1c = ind2 + side - 1;
+                    int p2c = ind2 - side + 1;
+                    int p1r, p2r;
+                    p1r = p2r = ind1 + side - 1;
 
-                    int sum = 0;
+                    int p3c = ind2;
+                    int p3r = p2r + side - 1;
 
-                    int x = i, y = j;
+                    bool s1 = (p1c<m) && (p2c<m) && (p3c<m);
+                    bool s2 = (p1r<n) && (p2r<n) && (p3r<n);
+                    bool s3 = (p1c>=0) && (p2c>=0) && (p3c>=0);
+                    bool s4 = (p1r>=0) && (p2r>=0) && (p3r>=0);
 
-                    // top -> right
-                    for(int t = 0; t < k; t++) {
-                        sum += grid[x + t][y + t];
+                    if(s1 && s2 && s3 && s4){
+                        int sum = 0;
+                        if(side==1){
+                            sum = grid[ind1][ind2];
+                        }
+                        else{
+                            int edge1 = d2[p1r][p1c] - d2[ind1][ind2];
+                            int edge2 = d1[p2r][p2c] - d1[ind1][ind2];
+                            int edge3 = d2[p3r][p3c] - d2[p2r][p2c];
+                            int edge4 = d1[p3r][p3c] - d1[p1r][p1c];
+                            
+                            sum = edge1 + edge2 + edge3 + edge4 + grid[ind1][ind2] - grid[p3r][p3c];
+                        }
+
+                        bool found = false;
+                        vector<int> temp; 
+                        while(!pq.empty()){
+                            if(pq.top() == sum) found = true;
+                            temp.push_back(pq.top());
+                            pq.pop();
+                        }
+                        for(int x : temp) pq.push(x);
+
+                        if (!found) {
+                            if(pq.size() < 3) {
+                                pq.push(sum);
+                            }
+                            else if (pq.top() < sum) {
+                                pq.pop();
+                                pq.push(sum);
+                            }
+                        }
+                        side++; 
                     }
-
-                    // right -> bottom
-                    for(int t = 0; t < k; t++) {
-                        sum += grid[x + k + t][y + k - t];
+                    else{
+                        break;
                     }
-
-                    // bottom -> left
-                    for(int t = 0; t < k; t++) {
-                        sum += grid[x + 2*k - t][y - t];
-                    }
-
-                    // left -> top
-                    for(int t = 0; t < k; t++) {
-                        sum += grid[x + k - t][y - k + t];
-                    }
-
-                    st.insert(sum);
                 }
             }
         }
 
         vector<int> ans;
-        for(auto it = st.rbegin(); it != st.rend() && ans.size() < 3; ++it) {
-            ans.push_back(*it);
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
         }
-
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
