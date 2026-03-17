9ba11302f0b1db4fc3f609c0251d14050bf86a45
Luv <ks753159@gmail.com>
2026-03-17 19:47:08 +0530

    Time: 288 ms (77.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,10 @@
+with cte as(
+    select requester_id as id from requestaccepted
+    union all
+    select accepter_id as id from requestaccepted
+)
+select id, count(*)as num
+from cte
+group by id
+order by count(*) desc
+limit 1;
