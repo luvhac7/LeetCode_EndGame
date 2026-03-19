7ba37400116f7ef6d33ca06ad593e8da48389007
Luv <ks753159@gmail.com>
2026-03-19 22:09:26 +0530

    Time: 666 ms (38.84%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,6 +1,6 @@
 select s.user_id,
-round(avg(case when c.action='confirmed'then 1 else 0 end),2)as confirmation_rate
+round(avg(case when action='confirmed' then 1 else 0 end),2) as confirmation_rate
 from signups s
-left join confirmations c
-on s.useR_id=c.useR_id
-group by s.useR_id
+left join confirmations c on c.user_id=s.user_id
+group by s.user_id
+order by confirmation_rate
