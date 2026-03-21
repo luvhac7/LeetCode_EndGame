9868d7c9551749e81406c76e5ea8ec4f89ca505c
Luv <ks753159@gmail.com>
2026-03-21 17:46:54 +0530

    Time: 607 ms (26.52%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,10 +1,10 @@
-select round(sum(a.tiv_2016),2) as tiv_2016
+select round(sum(a.tiv_2016),2)as tiv_2016
 from insurance a
 where a.tiv_2015 in(
     select b.tiv_2015 from insurance b
     where b.pid!=a.pid
 )
-and(a.lat,a.lon) not in(
-    select c.lat,c.lon from insurance c
-    where c.pid!=a.pid
+and (a.lat,a.lon) not in(
+    select x.lat,x.lon from insurance x
+    where x.pid!=a.pid
 )
