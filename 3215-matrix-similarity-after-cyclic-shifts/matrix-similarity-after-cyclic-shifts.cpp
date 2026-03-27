64c614e97e5e7c582e3a4d5070cf9f6348f3d8b0
Luv <ks753159@gmail.com>
2026-03-27 22:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (67.70%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    static bool areSimilar(vector<vector<int>>& mat, int k) {
-        int r=mat.size(),  c=mat[0].size();
-        k%=c;
-        if (k==0) return 1;
-        for(int i=0; i<r; i++){
-            bool iEven=(i&1)==0;
-            for(int j=0; j<c; j++)
-                if(mat[i][j]!=mat[i][(j+(iEven?c-k:k))%c]) return 0;
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        for(int i = 0; i < mat.size();i++){
+            for(int j = 0; j < mat.at(i).size(); j++){
+                if(mat.at(i).at((j+k)%mat.at(i).size()) != mat.at(i).at(j)){
+                    return false;
+                }
+            }
         }
-        return 1;
+        return true;
     }
 };
+
