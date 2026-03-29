5a5a994c4da8ca320fcfabc580182076c07d0c32
Luv <ks753159@gmail.com>
2026-03-29 21:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (14.84%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        string even1 = "";
-        even1 += s1[0];
-        even1 += s1[2];
-        string even2 = "";
-        even2 += s2[0];
-        even2 += s2[2];
-        sort(even1.begin(), even1.end());
-        sort(even2.begin(), even2.end());
-
-        if (even1 != even2) {
-            return false;
-        }
-        string odd1 = "";
-        odd1 += s1[1];
-        odd1 += s1[3];
-        string odd2 = "";
-        odd2 += s2[1];
-        odd2 += s2[3];
-        sort(odd1.begin(), odd1.end());
-        sort(odd2.begin(), odd2.end());
-        
-        if (odd1 != odd2) {
-            return false;
-        }
-
-        return true;
+      string a="";a+=s1[0];a+=s1[2];
+      string b="";b+=s2[0];b+=s2[2];
+      string c="";c+=s1[1];c+=s1[3];
+      string d="";d+=s2[1];d+=s2[3];
+      sort(a.begin(),a.end());
+      sort(b.begin(),b.end());
+      sort(c.begin(),c.end());
+      sort(d.begin(),d.end());
+    if(a!=b) return false;
+    if(c!=d) return false;
+return true;
     }
 };
