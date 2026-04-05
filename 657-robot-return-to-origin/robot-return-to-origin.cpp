9b76266330b1ccc16178720febba3b203915e5fa
Luv <ks753159@gmail.com>
2026-04-05 15:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (76.78%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool judgeCircle(string a) {
-        if(a.size()&1) return false;
+    bool judgeCircle(string s) {
+        if(s.size()&1) return false;
         int u=0,d=0,l=0,r=0;
-        for(char c:a)
+        for(char c:s)
         {
             u+=c=='U',d+=c=='D',l+=c=='L',r+=c=='R';
         }
