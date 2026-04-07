590fa4edbfa155f19f6aafd3cecf1f572fc3a4fe
Luv <ks753159@gmail.com>
2026-04-07 22:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (63.95%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -12,17 +12,39 @@ class Solution {
 public:
 typedef ListNode l;
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next || k==0) return head;
-        l* tail;
-        int len=1;
+        // if(!head || !head->next || k==0) return head;
+        // l* tail;
+        // int len=1;
+        // for(tail=head;tail->next;tail=tail->next)
+        //     ++len;
+        // tail->next=head;//circle bna
+        // int t=len-k%len;//us possition jispe rotaion karna h usko nikklo
+
+        // for(int i=0;i<t;i++)
+        //     tail=tail->next;
+        // l* newi=tail->next;
+        // tail->next=nullptr;
+        // return newi;
+        return f(head,k);
+    }
+private:
+    l* f(l* head,int k)
+    {
+        if(!head||k==0 ||!head->next)
+        {
+            return head;
+        }
+        l* tail;int x=1;
         for(tail=head;tail->next;tail=tail->next)
-            ++len;
-        tail->next=head;//circle bna
-        int t=len-k%len;
+            ++x;
+        tail->next=head;
+        int t=x-k%x;
         for(int i=0;i<t;i++)
             tail=tail->next;
-        l* newi=tail->next;
+        l* ans=tail->next;
         tail->next=nullptr;
-        return newi;
+        return ans;
+
+        
     }
 };
