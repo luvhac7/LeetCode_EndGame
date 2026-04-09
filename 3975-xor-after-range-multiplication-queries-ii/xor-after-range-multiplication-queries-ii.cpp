79af000716198fd69a91de271468c3eb9af0a6a6
Luv <ks753159@gmail.com>
2026-04-09 22:54:36 +0530

    Time: 160 ms (98.82%) | Memory: 203.5 MB (88.24%) - LeetSync

--- 3975-xor-after-range-multiplication-queries-ii/xor-after-range-multiplication-queries-ii.cpp
+++ 3975-xor-after-range-multiplication-queries-ii/xor-after-range-multiplication-queries-ii.cpp
@@ -1,80 +1,58 @@
 class Solution {
+    long long modInverse(long long a,long long MOD){
+        return modPower(a,MOD-2,MOD);
+    }
+    long long modPower(long long x, long long y, long long P){    
+        if( y == 0LL) return 1LL;
+        long long tmp = modPower(x, y/2LL, P);
+    
+        if(y&1) return (((x*tmp)%P)*tmp)%P ;
+        else return  ((tmp*tmp)%P) ;
+    }
+    int getOrDefault(map<pair<int,int>,int>& mp, pair<int,int> key, int defaultVal) {
+        auto it = mp.find(key);
+        if(it == mp.end()) return defaultVal;
+        return it->second;
+    }
 public:
-    static const int MOD = 1e9 + 7;
-
-    long long modPow(long long base, long long exp) {
-        long long result = 1;
-
-        while (exp > 0) {
-            if (exp & 1) {
-                result = (result * base) % MOD;
-            }
-
-            base = (base * base) % MOD;
-            exp >>= 1;
-        }
-
-        return result;
-    }
-
-    long long modInverse(long long x) {
-        return modPow(x, MOD - 2);
-    }
-
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        int limit = sqrt(n) + 1;
-
-        unordered_map<int, vector<vector<int>>> smallQueries;
-
-        for (auto &q : queries) {
-            int l = q[0];
-            int r = q[1];
-            int k = q[2];
-            int v = q[3];
-
-            if (k >= limit) {
-                for (int i = l; i <= r; i += k) {
+        int S = sqrt(n) + 1; 
+        vector<map<pair<int,int>,int>> events(S);
+        int MOD = 1e9 + 7;
+        for(auto& q: queries) {
+            int l = q[0], r = q[1], k = q[2], v = q[3];
+            if(k >= S) {
+                for(int i=l; i<=r; i+=k) {
                     nums[i] = (1LL * nums[i] * v) % MOD;
                 }
-            } else {
-                smallQueries[k].push_back(q);
+                continue;
+            }
+            // for smaller k
+            events[k][{l%k,l}] = (1LL * getOrDefault(events[k], {l%k,l}, 1) * v) % MOD;
+            int r2 = r + (k - (r - l)%k);
+            if(r2 < n) {
+                events[k][{r2%k,r2}] = (1LL* getOrDefault(events[k], {r2%k,r2}, 1) * modInverse(v, MOD)) % MOD;
             }
         }
-
-        for (auto &[k, group] : smallQueries) {
-            vector<long long> diff(n, 1);
-
-            for (auto &q : group) {
-                int l = q[0];
-                int r = q[1];
-                int v = q[3];
-
-                diff[l] = (diff[l] * v) % MOD;
-
-                int steps = (r - l) / k;
-                int nextPos = l + (steps + 1) * k;
-
-                if (nextPos < n) {
-                    diff[nextPos] = (diff[nextPos] * modInverse(v)) % MOD;
+        for(int k=1; k<S; k++){
+            map<pair<int,int>,int>& e = events[k];
+            auto it = e.begin();
+            while(it != e.end()) {
+                int start = (it)->first.second;
+                int multBy = 1;
+                int x = start % k;
+                for(int i=start; i<n; i+=k) {
+                    if(it->first.second == i) {
+                        multBy = (1LL * multBy * it->second) % MOD;
+                        ++it;
+                    }
+                    nums[i] = (1LL * nums[i] * multBy) % MOD;
                 }
             }
-
-            for (int i = 0; i < n; i++) {
-                if (i >= k) {
-                    diff[i] = (diff[i] * diff[i - k]) % MOD;
-                }
-
-                nums[i] = (1LL * nums[i] * diff[i]) % MOD;
-            }
         }
-
-        int answer = 0;
-
-        for (int num : nums) {
-            answer ^= num;
-        }
-
-        return answer;
+        int res = 0;
+        for(int x : nums) res ^= x;
+        return res;
     }
 };
