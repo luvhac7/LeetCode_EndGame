540515fffd16068a574e6ef74100002f3a18005f
Luv <ks753159@gmail.com>
2026-04-27 21:53:15 +0530

    Time: 293 ms (6.45%), Space: 126.6 MB (5.24%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> dx = {0,0,1,-1};
+    vector<int> dy = {1,-1,0,0};
+
+    bool valid(int x, int y, int m, int n) {
+        return x>=0 && y>=0 && x<m && y<n;
+    }
+
+    bool hasValidPath(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<vector<int>>> dir = {
+            {},
+            {{0,-1},{0,1}},
+            {{-1,0},{1,0}},
+            {{0,-1},{1,0}},
+            {{0,1},{1,0}},
+            {{0,-1},{-1,0}},
+            {{0,1},{-1,0}}
+        };
+
+        vector<vector<int>> vis(m, vector<int>(n,0));
+        queue<pair<int,int>> q;
+
+        q.push({0,0});
+        vis[0][0] = 1;
+
+        while (!q.empty()) {
+            auto [x,y] = q.front(); q.pop();
+
+            if (x==m-1 && y==n-1) return true;
+
+            for (auto d : dir[grid[x][y]]) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if (!valid(nx,ny,m,n) || vis[nx][ny]) continue;
+
+                for (auto back : dir[grid[nx][ny]]) {
+                    if (nx + back[0] == x && ny + back[1] == y) {
+                        vis[nx][ny] = 1;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
