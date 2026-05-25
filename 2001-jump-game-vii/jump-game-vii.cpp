f3cc213b18b760c95d6974fedfd0c9e578adffed
Luv <ks753159@gmail.com>
2026-05-25 17:21:05 +0530

    Time: 35 ms (7.49%) | Memory: 27.6 MB (31.41%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.cpp
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    bool canReach(string s, int minJ, int maxJ) {
-        int n = s.length();
+    bool canReach(string s, int minJump, int maxJump) {
+        // if any zero is invalid , move ahead , dont add it to the zero set 
+        vector<int> setz;
+        setz.push_back(0);
+        int n =s.size();
+        for(int i=1;i<n;i++){
+            if(s[i]=='0'){
+            int left =(i-maxJump);
+            int right=(i-minJump);
+            if(left<0 && right<0) continue;
+            else if(left<0 && right>=0){
+                left=0;
+            }
+            // we have the left and right  
+            // get first index where el>=left , and found in set 
+            int low =0;
+            int high =setz.size()-1;
+            while(low<=high){
+                int mid =low+(high-low)/2;
+                if(setz[mid]>=left){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }
 
-        if (s.back() & 1) return false;
+            // if(low<setz.size()) cout<<"low index "<<low<<endl;
 
-        s[0] = 'v';
-        int reach = 0, maxR = maxJ;
-
-        for (int i = minJ; i < n; i++) {
-            if (i > maxR) return false;
-
-            reach += s[i - minJ] == 'v';
-            reach -= (i > maxJ) && s[i - maxJ - 1] == 'v';
-
-            if (reach && (~s[i] & 1)) {
-                s[i] = 'v';
-                maxR = i + maxJ;
+            // check if low is valid 
+            // check if low is valid and index 
+            // cout<<"left and right "<<left<<" "<<right<<endl;
+            if(low<setz.size()){
+                // cout<<setz[low]<<" "<<endl;
+                if(setz[low]<=right && setz[low]>=left){
+                    // cout<<"valid "<<i<<endl;
+                    setz.push_back(i);
+                }
+            }
             }
         }
 
-        return reach;
+        // for(int el:setz) cout<<el<<" ";
+        // cout<<endl;
+        // check if the last index is n-1 
+        return setz[setz.size()-1]==n-1;
     }
 };
