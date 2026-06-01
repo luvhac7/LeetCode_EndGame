def2c3820627b172edd5eb84f05e6948906d5a37
Luv <ks753159@gmail.com>
2026-06-02 00:50:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (49.81%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int minimumCost(vector<int>& a) {
-        sort(a.begin(),a.end());int t=0;
-        for(int i=a.size()-1;i>=0;i-=3){
-            t+=a[i];
-            if(i-1>=0) t+=a[i-1];
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(),cost.end());
+        int total =0;
+
+        for(int i=cost.size()-1;i>=0;i-=3){
+            total += cost[i];
+            if(i-1>=0) total+=cost[i-1];
         }
-    return t;
+
+        return total;
     }
 };
