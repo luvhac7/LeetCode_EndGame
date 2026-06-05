5973a146a4629c93c39da7880cfdc3f164882682
Luv <ks753159@gmail.com>
2026-06-05 14:40:12 +0530

    Time: 23 ms (89.83%) | Memory: 9.4 MB (95.76%) - LeetSync

---
+++ 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    
+    struct Node {
+        long long waviness;
+        long long cnt;
+    };
+
+    string s;
+
+    Node dp[17][2][2][11][11];
+    bool vis[17][2][2][11][11];
+    Node dfs(int pos, int tight, int started, int prev2, int prev1) {
+        if (pos == s.size()) {
+            return {0, 1};
+        }
+        if (vis[pos][tight][started][prev2][prev1]) {
+            return dp[pos][tight][started][prev2][prev1];
+        }
+        vis[pos][tight][started][prev2][prev1] = true;
+
+        long long totalWaviness = 0;
+        long long totalCnt = 0;
+        int limit = tight ? s[pos] - '0' : 9;
+
+        for (int d = 0; d <= limit; d++) {
+            int nextTight = tight && (d == limit);
+
+            if (!started && d == 0) {
+                Node child = dfs(pos + 1,nextTight,0,10,10);
+
+                totalWaviness += child.waviness;
+                totalCnt += child.cnt;
+            }
+            else { 
+                int nPrev2, nPrev1;
+                long long add = 0;
+
+                if (!started) {
+                    nPrev2 = 10;
+                    nPrev1 = d;
+                }
+                else {
+                    if (prev2 != 10) {
+                        bool peak =(prev1 > prev2 && prev1 > d);
+                        bool valley =(prev1 < prev2 && prev1 < d);
+                        if (peak || valley) add = 1;
+                    }
+                    nPrev2 = prev1;
+                    nPrev1 = d;
+                }
+                Node child = dfs(pos + 1,nextTight,1,nPrev2,nPrev1);
+                totalWaviness += child.waviness + add * child.cnt;
+                totalCnt += child.cnt;
+            }
+        }
+
+        return dp[pos][tight][started][prev2][prev1] ={totalWaviness, totalCnt};
+    }
+
+    long long solve(long long n) {
+        if (n <= 0) return 0;
+        s = to_string(n);
+        memset(vis, 0, sizeof(vis));
+        return dfs(0, 1, 0, 10, 10).waviness;
+    }
+
+    long long totalWaviness(long long num1, long long num2) {
+        return solve(num2) - solve(num1 - 1);
+    }
+};
