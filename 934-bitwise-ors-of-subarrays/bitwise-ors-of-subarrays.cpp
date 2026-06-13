9972c232b0ced7cd0dfaada3e139a8b87b560ebe
Luv <ks753159@gmail.com>
2026-06-14 00:49:03 +0530

    Time: 1568 ms (9.49%) | Memory: 392.2 MB (5.19%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -13,10 +13,23 @@ int f(vector<int>&a)
     }
     return s.size();
 }
+int sol(vector<int>&a)
+{set<int>st1,st2;
+for(int i:a)
+{
+    set<int>temp;
+    st2.insert(i);
+    for(int j:st2) temp.insert(i|j); 
+    st2=temp;
+    for(int j:temp) st1.insert(j); 
+}
+return st1.size();
+
+}
     int subarrayBitwiseORs(vector<int>& a) {  
         // int n=a.size();
         // if(n==1) return 1;
-        return f(a);
+        return sol(a);
         // else return a[n-1]+a[n-2];
     }
 };
