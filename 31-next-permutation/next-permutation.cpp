60204096a4a92cc2f4816feb8e1ea0064e5cefdf
Luv <ks753159@gmail.com>
2026-06-15 09:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.26%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int n=a.size();
+        int i=n-2;
+        while(i>=0 && a[i]>=a[i+1])i--;
+        if(i>=0){
+            int j=n-1;
+            while(a[j]<=a[i]){
+                j--;
+            }
+            swap(a[i],a[j]);
+        }
+        reverse(a.begin()+i+1,a.end());
+    }
+};
