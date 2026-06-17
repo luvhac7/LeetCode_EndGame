b8ae76619c812a3fd4abf1eb84bae7dcb216aeb8
Luv <ks753159@gmail.com>
2026-06-17 12:26:45 +0530

    Time: 14 ms (10.11%) | Memory: 24.2 MB (7.49%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int singleNumber(vector<int>& a) {
+        //1
         // map<int,int>f;for(int i:a) f[i]++;
         // for(auto &i:f){
         //     if(i.second==1) return i.first;
         // }
         // return -1;
-        int x=0;
+        //2
+        // int x=0;
+        // for(int i:a)
+        //     x^=i;
+        // return x;
+        //3
+        set<int>st;
         for(int i:a)
-            x^=i;
-        return x;
+        {
+            if(st.find(i)!=st.end())
+            {
+                st.erase(i);
+            }
+            else {
+                st.insert(i);
+            }
+        }
+        return *st.begin();
     }
 };
