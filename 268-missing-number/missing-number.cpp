badb7b80722597210365f39bf66f9477035e2483
Luv <ks753159@gmail.com>
2026-06-17 23:12:02 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.70%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,12 +5,24 @@ public:
         // for(int i=0;i<a[n-1];i++)
         //     if(a[i]!=i) return i;
         // return a[n-1]+1;
-        int s=0,n=a.size();
-        int ex=n*(n+1)/2;
-        for(int i:a)
+        //without sorting
+        // int s=0,n=a.size();
+        // int ex=n*(n+1)/2;
+        // for(int i:a)
+        // {
+        //     s+=i;
+        // }
+        // return ex-s;
+        //using xor
+        int n=a.size(),x=0;
+        for(int i=1;i<=n;i++)
         {
-            s+=i;
+            x^=i;
         }
-        return ex-s;
+        for(int i=0;i<n;i++)
+        {
+            x^=a[i];
+        }
+        return x;
     }
 };
