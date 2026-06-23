9c66ea0bec8b3e511e837f4bed27210998420e79
Luv <ks753159@gmail.com>
2026-06-23 13:34:06 +0530

    Time: 255 ms (75.93%) | Memory: 13.2 MB (65.74%) - LeetSync

--- 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.cpp
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
 int M=1e9+7;
+typedef long long ll;
+int f(int n,int l,int r)
+{
+int m=r-l+1;
+vector<ll>dp(m,1);
+while(--n)
+{
+    reverse(dp.begin(),dp.end());
+    ll pre=0;
+    for(auto &x:dp)
+    {
+    ll cur=x;
+    x=pre;
+    pre=(pre+cur)%M;
+    }
+}
+ll ans=0;
+for(auto x:dp)ans=(ans+x)%M;
+return (ans*2)%M;
+}
     int zigZagArrays(int n, int l, int r) {
-        int m=r-l+1;
-        vector<int>a(m,1);
-        for(int i=2;i<=n;i++)
-        {
-            reverse(a.begin(),a.end());
-            int sum=0;
-            for(auto &it:a)
-            {
-            sum=(sum+exchange(it,sum))%M;
-        }
-        }
-        return ((accumulate(a.begin(),a.end(),0LL)%M)<<1)%M;
+    return f(n,l,r);
     }
 };
