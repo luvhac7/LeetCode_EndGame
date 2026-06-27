7228dd21898f562ae455e519dafe8ae4d0ea8b7d
Luv <ks753159@gmail.com>
2026-06-27 20:08:24 +0530

    Time: 131 ms (5.33%), Space: 22.7 MB (74.88%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+typedef long long ll;
+bool f(int n)
+{
+    if(n<2) return false;
+    for(int i=2;i*i<=n;i++) if(n%i==0) return false;
+    return true;
+}
+bool p(set<int>&st,int n)
+{
+    if(n<2)return false;
+    for(int i=2;i<=n;i++)
+    {
+        if(n%i==0 && f(i))
+        {
+            st.insert(i);
+        }
+    }
+    return true;
+}
+    int distinctPrimeFactors(vector<int>& a ) {
+        // ll p=1;set<int>st;
+        // for(int i:a) p*=i;
+        // for(int i=1;i<1000;i++)
+        // {
+        //     for(int j=0;j<a.size();j++)
+        //     {
+        //        if(a[j]%i) st.insert(a[j]);
+        //     }
+        // }
+        // return st.size();
+    //     vector<int> factors;
+    //     int n=a.size();
+    // // Loop only up to the square root of n
+    // for (int i = 1; i * i <= n; ++i) {
+    //     if (n % i == 0) {
+    //         factors.push_back(i); // Add the smaller factor
+            
+    //         // Avoid duplicating the square root if it is a perfect square
+    //         if (i != n / i) {
+    //             factors.push_back(n / i); // Add the matching larger factor
+    //         }
+    //     }
+    // }
+    //     set<int>st;
+    //     for(int i:factors) st.insert(i);
+    //     return st.size();
+  set<int>st;int n=a.size();
+// for(int n:a){  for(int i=2;i*i<=n;i++)
+//   {
+//     if(n%i==0)
+//     {
+//         st.insert(i);
+//         while(n%i==0)
+//         {
+//             n/=i;
+//     }
+//     }
+//   }
+//   if(n>1) st.insert(n);
+  for(int i:a) p(st,i);
+return st.size();
+    }
+};
