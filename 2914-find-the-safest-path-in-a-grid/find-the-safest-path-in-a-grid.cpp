6416318b7875ea36a464f5ceb6c29bc5376bb6f8
Luv <ks753159@gmail.com>
2026-07-01 10:14:57 +0530

    Time: 1573 ms (5.06%) | Memory: 433.5 MB (5.06%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,81 +1,91 @@
 class Solution {
-public:
-    bool isValid(const vector<vector<int>>& dist, int safeness) {
-        int n = dist.size();
-        if (dist[0][0] < safeness || dist[n - 1][n - 1] < safeness) return false;
+    const vector<vector<int>> dirs = {
+        {1,0},
+        {-1,0},
+        {0,1},
+        {0,-1}
+    };
+    int n;
 
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-        visited[0][0] = true;
-
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
-
-        while (!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
-
-            if (x == n - 1 && y == n - 1) return true;
-
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny] && dist[nx][ny] >= safeness) {
-                    visited[nx][ny] = true;
-                    q.push({nx, ny});
-                }
-            }
-        }
-        return false;
+    bool canVisit(int i, int j) {
+        if (i<0 || j<0 || i>=n || j>=n)
+            return false;
+        return true;
     }
 
-    int maximumSafenessFactor(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        queue<pair<int, int>> q;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    dist[i][j] = 0;
-                    q.push({i, j});
+    void calculateSafeness(vector<vector<int>> &safeness, const vector<vector<int>>& grid) {
+        queue<pair<int,int>> bfs;
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j]==1) {
+                    bfs.push({i,j});
+                    safeness[i][j]=0;
                 }
             }
         }
 
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
+        int layer, depth=0, i, j, ni, nj;
 
-        while (!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
+        while (!bfs.empty()) {
+            layer=bfs.size();
+            depth++;
+            while (layer--) {
+                i=bfs.front().first;
+                j=bfs.front().second;
+                bfs.pop();
 
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                    if (dist[nx][ny] > dist[x][y] + 1) {
-                        dist[nx][ny] = dist[x][y] + 1;
-                        q.push({nx, ny});
+                for (vector<int> dir : dirs) {
+                    ni=i+dir[0];
+                    nj=j+dir[1];
+                    if (canVisit(ni, nj) && safeness[ni][nj]==-1) {
+                        bfs.push({ni,nj});
+                        safeness[ni][nj]=depth;
                     }
                 }
             }
         }
-        int low = 0, high = dist[0][0];
-        int ans = 0;
+    }
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isValid(dist, mid)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        n=grid.size();
+        vector<vector<int>> safeness(n, vector<int>(n, -1));
+
+        calculateSafeness(safeness, grid);
+
+        vector<vector<int>> parent(n, vector<int>(n, -2));
+        priority_queue<pair<int,pair<int,int>>> pq;
+
+        parent[0][0]=-1;
+        pq.push({safeness[0][0], {0,0}});
+        int i, j, ni, nj;
+
+        while (parent[n-1][n-1]==-2) {
+            i = pq.top().second.first;
+            j = pq.top().second.second;
+            pq.pop();
+
+            for (int d=0; d<4; d++) {
+                ni=i+dirs[d][0];
+                nj=j+dirs[d][1];
+
+                if (canVisit(ni, nj) && parent[ni][nj]==-2) {
+                    parent[ni][nj]=d;
+                    pq.push({safeness[ni][nj], {ni,nj}});
+                }
             }
         }
 
-        return ans;
+        i=n-1;
+        j=n-1;
+        int res=safeness[i][j], d;
+        while (parent[i][j]>=0) {
+            d=parent[i][j];
+            i-=dirs[d][0];
+            j-=dirs[d][1];
+            res=min(res, safeness[i][j]);
+        }
+        return res;
     }
 };
