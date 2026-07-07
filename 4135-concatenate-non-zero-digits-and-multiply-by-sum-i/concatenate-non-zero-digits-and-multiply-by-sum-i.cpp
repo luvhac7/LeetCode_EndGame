50ef9714cdaf356453c234ad34086ec6190dbf37
Luv <ks753159@gmail.com>
2026-07-06 21:29:08 -0700

    Time: 0 ms (100.00%) | Memory: 9.1 MB (51.79%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-typedef long long ll;
     long long sumAndMultiply(int n) {
-        vector<int>d;ll no=n,s=0,p=0;
-        while(n>0)
+        long long x=0;
+        int sum=0;
+        long long place=1;
+        while(n>0)      
         {
-        if(n%10 != 0){
-            d.push_back(n%10);
+            int digit=n%10;
+            if(digit!=0)
+            {
+                x+=digit * place;
+                place*=10;
+                sum+=digit;
+            }
+            n/=10;
         }
-        n/=10;
-        }
-        if(d.empty()) return 0;
-        for(int i:d)
-        {
-            p+=i;
-        }
-        string res="";
-        for(int i:d)
-        {
-            res+=to_string(i);
-            cout<<res<<" ";
-        }
-        reverse(res.begin(),res.end());
-    return (ll)p*std::stoll(res);
+        return x*sum;
     }
 };
