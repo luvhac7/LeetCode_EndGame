474e6446c1fa84e3dd6d91e06faf6d827b5a8437
Luv <ks753159@gmail.com>
2026-07-29 08:54:06 +0530

    Time: 285 ms (5.80%) | Memory: 17.8 MB (30.43%) - LeetSync

--- 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
@@ -1,82 +1,68 @@
-constexpr int INF=1e6+1, N=24;
-int C[N][N]={{0}};
-
-constexpr void Pascal() {
-    if (C[0][0]==1) return; // computed once
-    C[0][0]=1;
-    for (int i=1; i<N; i++) {
-        C[i][0]=C[i][i]=1;
-        for (int j=1; j<=i/2; j++) {
-            C[i][j]=C[i][i-j]=C[i-1][j-1]+C[i-1][j];
-        }
-    }
-}
-
 class Solution {
 public:
-    static int comb(int n, int k) {
-        if (n<N) return C[n][k];//  look at the table
-        if (2*k>n) k=n-k;// C(n, n-k)=C(n, k)
-        int64_t ans=1;
-        for (int i=1; i<=k; i++) {
-            ans=ans*(n-i+1)/i;
-            if (ans>=INF) return INF;
+    string smallestPalindrome(string s, int k) {
+        int n=s.size();
+        vector<int>cnt(26,0);
+        for(char c:s)cnt[c-'a']++;
+        int mid=-1;
+        vector<int>half(26,0);
+        int halflen=0;
+        for(int i=0;i<26;i++){
+            if(cnt[i]&1)mid=i;
+            half[i]=cnt[i]/2;
+            halflen+=half[i];
         }
-        return ans;
-    }
-
-    static int perm(const array<int, 26>& freq, int sz) {
-        int64_t ans=1;
-        for (int f : freq) {
-            if (f==0) continue;
-            ans*=comb(sz, f);
-            if (ans>=INF) return INF;
-            sz-=f;
-        }
-        return ans;
-    }
-
-    static string smallestPalindrome(string& s, int k) {
-        Pascal();
-        const int n=s.size(), n0=n/2;
-        array<int, 26> freq={0};
-        for (int i=0; i<n0; i++)
-            freq[s[i]-'a']++;
-        
-        int total=perm(freq, n0);
-        if (k>total) return "";
-
-        string left;
-        left.reserve(n);
-        int sz=n0;
-        for (int i=0; i<n0; i++) {
-            for (int c=0; c<26; c++) {
-                if (freq[c]==0) continue;
-                freq[c]--;
-                int cnt=perm(freq, --sz);
-                if (cnt>=k) {
-                    left.push_back('a' + c);
-                    break;
-                } 
-                else {
-                    k -= cnt;
-                    freq[c]++;// backtrack
-                    sz++;
+        long long cap=2e6;
+        auto multinom=[&](vector<int>&h)->long long{
+            int total=0;
+            for(int x:h)total+=x;
+            long long res=1;
+            int used=0;
+            for(int i=0;i<26;i++){
+                if(h[i]==0)continue;
+                long long c=1;
+                int nn=total-used,rr=h[i];
+                for(int t=1;t<=rr;t++){
+                    c=c*(nn-rr+t)/t;
+                    if(c>cap){
+                        c=cap;
+                        break;
+                    }
                 }
+    res*=c;
+    if(res>cap)res=cap;
+    used+=h[i];
+            }
+
+            return res;
+        };
+       if(multinom(half)<k)return "";
+       string lefthalf="";
+       long long kk=k;
+       for(int pos=0;pos<halflen;pos++){
+        for(int c=0;c<26;c++){
+            if(half[c]==0)continue;
+            half[c]--;
+
+            long long ways=multinom(half);
+            if(kk<=ways){
+                lefthalf+=('a'+c);
+                break;
+            }
+            else {
+                kk-=ways;
+                half[c]++;
             }
         }
 
-        string right=left;
-        reverse(right.begin(), right.end());
-        if (n&1) left.push_back(s[n/2]);
-        left.append(right);
-        return left;
+
+       }
+       string result=lefthalf;
+       if(mid!=-1)result+=('a'+mid);
+       string rev=lefthalf;
+       reverse(rev.begin(),rev.end());
+       result+=rev;
+       return result;
+      
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
