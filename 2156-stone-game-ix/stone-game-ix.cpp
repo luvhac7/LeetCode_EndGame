bfaf528c82320aa6f1a1a478a9b42dca50b3530b
Luv <ks753159@gmail.com>
2026-08-16 17:23:43 +0530

    Time: 4 ms (62.44%) | Memory: 131.2 MB (92.89%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.cpp
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
+bool f(vector<int>&a)
+{
+    vector<int>cnt(3,0);
+    for(int i:a) cnt[i%3]++;
+    if(cnt[0]%2==0) return cnt[1]>0 && cnt[2]>0;
+    else return abs(cnt[1]-cnt[2])>2;
+}
     
-    bool stoneGameIX(vector<int>& stones) {
-        vector<int> count(3,0);
-        for(int stone : stones){
-            count[stone%3]++;
-        }
-        if(count[0]%2 == 0){
-            return count[1]>0 && count[2]>0;
-        }else{
-            return abs(count[1] - count[2])>2;
-        }
+    bool stoneGameIX(vector<int>& a) {
+      return f(a);
     }
 };
