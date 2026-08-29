191f0ffa5916a59472018719a57151cce986a19f
Luv <ks753159@gmail.com>
2026-08-29 17:12:48 +0530

    Time: 730 ms (5.35%) | Memory: 187.8 MB (54.37%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,37 +1,52 @@
 class Solution {
+private:
+    vector<int> parent{};
+    int find(int i){
+        if(parent[i]==-1)return i;
+        return find(parent[i]);
+    }
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        int n = nums.size();
-
-        vector<int> sorted = nums;
-        sort(sorted.begin(), sorted.end());
-
-        unordered_map<int, vector<int>> group;
-        unordered_map<int, int> groupId;
-        unordered_map<int, int> pos;
-
-        int id = 1;
-        group[id].push_back(sorted[0]);
-        groupId[sorted[0]] = id;
-
-        for(int i = 1; i < n; i++){
-            if(sorted[i] - sorted[i - 1] > limit){
-                id++;
+        using tuple_t = typename std::tuple<int,int>;
+        #define g0 std::get<0>
+        #define g1 std::get<1>
+        #define mt std::make_tuple
+        const auto n{int(size(nums))};
+        vector<tuple_t> a{};
+        a.reserve(size(nums));
+        int i{-1};
+        ranges::transform(nums,back_inserter(a),[&](const auto x){
+            return mt(x,++i);
+        },{});
+        ranges::sort(a);
+        //{is....ie}
+        multiset<int> dq[100'001ull]{};
+        parent.assign(n,-1);
+        int j{g1(a[0])};
+        for(int i{1};i<n;++i){
+            if(g0(a[i])-g0(a[i-1]) <= limit){
+                parent[g1(a[i])]=j;
             }
-
-            group[id].push_back(sorted[i]);
-            groupId[sorted[i]] = id;
+            else{
+                j=g1(a[i]);
+            }
         }
-
-        // Rebuild nums using the smallest
-        // available value from its group
-        for(int i = 0; i < n; i++){
-            int grp = groupId[nums[i]];
-
-            nums[i] = group[grp][pos[grp]];
-            pos[grp]++;
+        if(0){
+            ranges::copy(parent,ostream_iterator<int>(cout," "));
+            cout<<"\n";
+            return nums;
         }
-
+        for(int i{};i<n;++i){
+            dq[find(g1(a[i]))].emplace(g0(a[i]));
+        }
+        ranges::sort(a,[]<typename T>(const T& x,const T& y){
+            return g1(x)<g1(y);
+        });
+        for(int i{};i<n;++i){
+            auto&& set{dq[find(i)]};
+            nums[i]=*ranges::begin(set);
+            set.erase(begin(set));
+        }
         return nums;
     }
 };
