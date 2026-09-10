3e4a19edcbc92ff5f6ff220cc6ab3746730694a7
Luv <ks753159@gmail.com>
2026-09-10 14:06:56 +0530

    Time: 34 ms (5.69%) | Memory: 15.7 MB (84.80%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -1,30 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int count = 0;
-    
-    pair<int, int> postOrder(TreeNode* root) {
-        if (root == NULL) {
-            return {0, 0};
-        }
-        
-        // First iterate over left and right subtrees.
-        pair<int, int> left = postOrder(root->left);
-        pair<int, int> right = postOrder(root->right);
-        
-        int nodeSum = left.first + right.first + root->val;
-        int nodeCount = left.second + right.second + 1;
-
-        // Check if the average of the subtree is equal to the node value.
-        if (root->val == nodeSum / (nodeCount)) {
-            count++;
-        }
-        
-        // Return the sum of nodes and the count in the subtree.
-        return {nodeSum, nodeCount};
+int sum(TreeNode* root){
+    if(root==NULL)return 0;
+    int l=sum(root->left);
+    int r=sum(root->right);
+    return root->val+l+r;
+}
+int count_nodes(TreeNode* root){
+    if(root==NULL)return 0;
+    int l=count_nodes(root->left);
+    int r=count_nodes(root->right);
+    return 1+l+r;
+}
+int f(TreeNode* root){
+    if(root==NULL)return 0;
+    int curr=0;
+    int tar=sum(root)/count_nodes(root);
+    if(root->val==tar){
+        curr=1;
     }
-    
+    int l=f(root->left);
+    int r=f(root->right);
+    return curr+l+r;
+}
     int averageOfSubtree(TreeNode* root) {
-        postOrder(root);
-        return count;
+        int res=f(root);
+        return res;       
     }
 };
