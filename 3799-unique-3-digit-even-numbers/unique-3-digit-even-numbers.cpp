edb284a8a9d820fb5798477e16dfa5eead2e1c45
Luv <ks753159@gmail.com>
2026-09-11 15:30:27 +0530

    Time: 85 ms (6.76%) | Memory: 61.9 MB (6.76%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        int n = digits.size();
-        int even = 0, z = 0;
-        unordered_map <int, int> mpp;
-        for (auto i : digits) mpp[i] += 1;
-        unordered_map <int, int> temp(mpp.begin(), mpp.end());
         int count = 0;
-        for (int i = 100;i < 1000;i += 2) {
-            int val = i;
-            bool flag = true;
-            while (val != 0) {
-                int rem = val % 10;
-                if (mpp.count(rem)) {
-                    mpp[rem] -= 1;
-                    if (mpp[rem] == 0) mpp.erase(rem);
-                } else {
-                    flag = false;
-                    break;
-                }
-                val /= 10;
+        vector <int> freq (10, 0);
+        for(int num : digits){
+            freq[num]++;
+        } 
+
+        for(int i = 100; i <= 998; i += 2){
+            int a = i / 100;
+            int b = (i / 10) % 10;
+            int c = i % 10;
+
+            vector<int> temp = freq;
+
+            temp[a]--;
+            temp[b]--;
+            temp[c]--;
+
+            if(temp[a] >= 0 && temp[b] >= 0 && temp[c] >= 0){
+                count++;
             }
-            mpp = temp;
-            if (flag) count += 1;
         }
         return count;
     }
