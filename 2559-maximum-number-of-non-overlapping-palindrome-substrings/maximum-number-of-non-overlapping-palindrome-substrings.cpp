993035921072ec83830915c2d1240b2018829c08
Luv <ks753159@gmail.com>
2026-09-16 02:19:19 +0530

    Time: 1775 ms (5.87%) | Memory: 208.2 MB (17.60%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
+    bool check(int i, int j, string &s, vector<vector<int>> &p) {
+        if (p[i][j] != -1) {
+            return p[i][j];
+        }
+        while (i < j) {
+            if (s[i] != s[j]) {p[i][j] = 0;return false;
+            }
+            i++;
+            j--;
+        }
+        p[i][j] = 1;
+        return true;
+    }
+
     int maxPalindromes(string s, int k) {
         int n = s.size();
-        if (k == 1) return n;
-
-        int res = 0;
-
-        for (int i = 0; i <= n - k;) {
-            int step = 1;
-
-            for (int d : {k, k + 1}) {
-                if (i + d <= n && equal(s.begin() + i, s.begin() + i + (d >> 1),
-                                        s.rbegin() + (n - (i + d)))) {
-                    step = d;
-                    res++;
-                    break;
+        if(k==1)return n;
+        vector<int> dp(n + 1, 0);
+        vector<vector<int>> a(n, vector<int>(n, -1));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int ntake=dp[ind+1];
+            int take=0;
+            for (int i = ind; i < n; i++) {
+                if ((i - ind + 1) >= k && check(ind, i, s, a)) {
+                   take = max(take, 1 + dp[i + 1]);
                 }
             }
-
-            i += step;
+            dp[ind] = max(take, ntake);
         }
-
-        return res;
+        return dp[0];
     }
 };
