8cb8376e8be25abb2f48c40df9505780fe910146
Luv <ks753159@gmail.com>
2026-09-18 01:55:42 +0530

    Time: 681 ms (5.34%) | Memory: 217.3 MB (5.85%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -2,36 +2,51 @@ class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int n = arr.size();
-        int INF = n + 1;
-
-        vector<int> minLen(n, INF);
-
-        int left = 0;
-        long long sum = 0;
-        int best = INF;
-        int ans = INF;
-
-        for (int right = 0; right < n; right++) {
-            sum += arr[right];
-
-            while (sum > target) {
-                sum -= arr[left];
-                left++;
-            }
-
-            if (sum == target) {
-                int len = right - left + 1;
-
-                if (left > 0 && minLen[left - 1] != INF) {
-                    ans = min(ans, len + minLen[left - 1]);
-                }
-
-                best = min(best, len);
-            }
-
-            minLen[right] = best;
+        int ans = INT_MAX;
+        vector<int> prefSum(n) , suffSum(n);
+        prefSum[0] = arr[0];
+        suffSum[n-1] = arr[n-1];
+        for(int i = 1 ; i < n ; i++){
+            prefSum[i] = prefSum[i-1] + arr[i];
+        }
+        for(int i = n-2 ; i >= 0; i--){
+            suffSum[i] = suffSum[i+1] + arr[i];
         }
 
-        return ans == INF ? -1 : ans;
+        map<int,int> mpp;
+        mpp[0] = -1;
+
+        vector<int> leftBest(n,0),rightBest(n,0);
+        int left  = 1e9;
+        int right = 1e9;
+        for(int i = 0 ; i < n ; i++){
+            int rem = prefSum[i] - target;
+            if(mpp.find(rem) != mpp.end()){
+                left = min(left , i - mpp[rem]);
+            }
+            leftBest[i] = left;
+            mpp[prefSum[i]] = i;
+        }
+
+        map<int,int> mpp2;
+        mpp2[0] = n;
+        for(int i = n-1 ; i >= 0 ; i--){
+            int rem = suffSum[i] - target;
+            if(mpp2.find(rem) != mpp2.end()){
+                right = min(right ,mpp2[rem] - i);
+            }
+            rightBest[i] = right;
+            mpp2[suffSum[i]] = i;
+        }
+
+        for(int i = 0 ; i < n - 1; i++){
+            int leftSubarr  = leftBest[i];
+            int rightSubarr = rightBest[i+1];
+            if(leftSubarr == 1e9 || rightSubarr == 1e9) continue;
+
+            ans = min( ans , leftSubarr + rightSubarr);
+        }
+        if(ans == INT_MAX) return -1;
+        return ans;
     }
 };
